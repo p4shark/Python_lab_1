98384f5f24bf313de62cdf8241ee1f6bac33a504
p4shark <55730859+p4shark@users.noreply.github.com>
2021-01-19 19:34:25 +0500

    ver 1.2

--- main.py
+++ main.py
@@ -1,2 +1,17 @@
-print('hi')
-print('hiqwwwwwwwwwwwwwww')
+import glob
+import sys
+wr = open('test\output.txt','w+')
+i=0
+path ='test\*.dat'
+for f in glob.glob(path):
+     max = sys.float_info.min
+     with open(f, 'r') as f1:
+      for s in f1:
+       s=s[15:28]
+       s = s.replace('D', 'E')
+       s = float(s)
+       print s
+       if (s>max): max=s
+     i=i+1
+     wr.write('Max in 2 column in '+str(i)+' file: '+str(max)+'\n')
+wr.close()
